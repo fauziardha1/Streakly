56c9d937d8fed9a32d7397e38805665c3ffa0807
fauziardha1 <fauziardasaputra@gmail.com>
2024-10-07 23:40:02 +0700

    Initial Commit

---
+++ Streakly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Streakly
+//
+//  Created by FauziArda on 07/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
